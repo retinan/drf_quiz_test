cb16d8094c8af188b822d42649ca5bca962a4962
retin <voicemin78@gmail.com>
2023-09-07 21:37:18 +0900

    1. secret key 분리, 2. 디버그 false 3. DB PostgreSQL(by elephantsql) setup

--- quiz_app/settings.py
+++ quiz_app/settings.py
@@ -92,12 +92,24 @@ WSGI_APPLICATION = 'quiz_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'whftzvdw',
+        'USER': 'whftzvdw',
+        'PASSWORD': 'k5hppIfclZDeylm6u8uWfWaZ8uu26aqr',
+        'HOST': 'floppy.db.elephantsql.com',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
